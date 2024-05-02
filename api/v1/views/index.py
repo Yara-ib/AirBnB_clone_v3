8088a79a918ee2d856a24dd69c8e6588723306a2
Yara-ib <yr.ap.23@gmail.com>
2024-05-02 18:49:20 +0300

    Counting instances

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,7 +2,12 @@
 """ Index Module """
 from api.v1.views import app_views
 from flask import jsonify
-from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -20,6 +25,8 @@ def route_stats():
     Handling "stats" Route
     Returns: Count for each class object
     """
+    from models import storage
+
     return jsonify({
         "amenities": storage.count(Amenity),
         "cities": storage.count(City),
